34e5cfb5445b3a8e42f2173fea557a5a5955d730
VaibhavVhankade05 <vaibhavvhankade27@gmail.com>
2025-08-01 12:17:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <h1>Hello </h1>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
